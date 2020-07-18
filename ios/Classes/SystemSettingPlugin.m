4fd78e0d27750edbd4d7f81d7c893dab6d6822da
Thiago Porto <thiagonevesporto@hotmail.com>
2020-07-18 18:22:20 -0300

    Fixed swift file import

--- ios/Classes/SystemSettingPlugin.m
+++ ios/Classes/SystemSettingPlugin.m
@@ -1,5 +1,12 @@
 #import "SystemSettingPlugin.h"
+#if __has_include(<system_setting/system_setting-Swift.h>)
 #import <system_setting/system_setting-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "system_setting-Swift.h"
+#endif
 
 @implementation SystemSettingPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
